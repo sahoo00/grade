17458e96046e09f14f4655e56f22473edc969c3d
Debashis Sahoo <dsahoo@ucsd.edu>
2017-11-02 08:14:14 -0700

    sql

---
+++ print.sql
@@ -0,0 +1,7 @@
+select * from students;
+select * from scans;
+select * from params;
+select * from students WHERE lastName LIKE '%al%' LIMIT 3;
+select * from template;
+select * from rubric;
+select * from grades;
---
+++ schemas.sql
@@ -0,0 +1,56 @@
+DROP TABLE students;
+CREATE TABLE students 
+(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+lastName text,
+firstName text,
+userName text,
+studentID text,
+email text,
+grade REAL,
+uniqueID text
+);
+DROP TABLE scans;
+CREATE TABLE scans 
+(
+id INTEGER,
+page INTEGER,
+filename text,
+studentID INTEGER,
+PRIMARY KEY (id, page)
+);
+DROP TABLE params;
+CREATE TABLE params 
+(
+paramKey text,
+valueText text,
+valueInt INTEGER,
+PRIMARY KEY (paramKey)
+);
+DROP TABLE template;
+CREATE TABLE template 
+(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name text,
+value REAL,
+page INTEGER
+);
+DROP TABLE rubric;
+CREATE TABLE rubric 
+(
+id INTEGER,
+rid INTEGER,
+name text,
+value REAL,
+PRIMARY KEY (id, rid)
+);
+DROP TABLE grades;
+CREATE TABLE grades 
+(
+scanid INTEGER,
+tid INTEGER,
+notes text,
+value REAL,
+rlist text,
+PRIMARY KEY (scanid, tid)
+);
